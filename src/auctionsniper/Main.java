766faf5f972d5360242c810cb0736b6099a8f2eb
coconan <coconan8@gmail.com>
2019-12-05 11:27:37 +0800

    refactor auction

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -45,16 +45,10 @@ public class Main implements SniperListener {
         final Chat chat = connection.getChatManager().createChat(auctionId(itemId, connection), null);
         this.notToBeGCd = chat;
 
-        Auction auction = amount -> {
-            try {
-                chat.sendMessage(String.format(BID_COMMAND_FORMAT, amount));
-            } catch (XMPPException e) {
-                e.printStackTrace();
-            }
-        };
+        Auction auction = new XMPPAuction(chat);
         chat.addMessageListener(new AuctionMessageTranslator(new AuctionSniper(auction, this)));
 
-        chat.sendMessage(JOIN_COMMAND_FORMAT);
+        auction.join();
     }
 
     @Override
@@ -90,5 +84,30 @@ public class Main implements SniperListener {
 
     private void startUserInterface() throws Exception {
         SwingUtilities.invokeAndWait(() -> ui = new MainWindow());
+    }
+
+    public static class XMPPAuction implements Auction {
+        private final Chat chat;
+
+        public XMPPAuction(Chat chat) {
+            this.chat = chat;
+        }
+
+        @Override
+        public void bid(int amount) {
+            sendMessage(String.format(BID_COMMAND_FORMAT, amount));
+        }
+
+        public void join() {
+            sendMessage(JOIN_COMMAND_FORMAT);
+        }
+
+        private void sendMessage(final String message) {
+            try {
+                chat.sendMessage(message);
+            } catch (XMPPException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
