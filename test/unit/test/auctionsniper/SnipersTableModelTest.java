06e5ec1d011e9dbaa821e25e2be53c8c4513066f
coconan <coconan8@gmail.com>
2019-12-06 17:09:20 +0800

    refactor Column to be Object-Oriented

--- test/unit/test/auctionsniper/SnipersTableModelTest.java
+++ test/unit/test/auctionsniper/SnipersTableModelTest.java
@@ -47,7 +47,7 @@ public class SnipersTableModelTest {
         assertColumnEquals(Column.ITEM_IDENTIFIER, "item id");
         assertColumnEquals(Column.LAST_PRICE, 555);
         assertColumnEquals(Column.LAST_BID, 666);
-        assertColumnEquals(Column.SNIPER_STATUS, SnipersTableModel.textFor(SniperState.BIDDING));
+        assertColumnEquals(Column.SNIPER_STATE, SnipersTableModel.textFor(SniperState.BIDDING));
     }
 
     private void assertColumnEquals(Column column, Object expected) {
